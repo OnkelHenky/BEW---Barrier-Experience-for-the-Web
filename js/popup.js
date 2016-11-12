d80a784920f4f3d3fabb062faaad1a4fba7e0727
Kirr <kirrkev@outlook.de>
2016-11-12 12:02:38 +0100

    Dynamischer Wechsel zwischen Kategorien
    - Es wurden weitere Json Files hinzugefügt
    - Die Kategorien in der Extension können dynamisch gewechselt werden.

--- js/popup.js
+++ js/popup.js
@@ -1,25 +1,34 @@
 //Parse JSON to String Array
-function getJSONdata()
-{   
-  $.getJSON("../json/clarifications.json", function(json) {
-    //console.log("Content of JSON.file", json);
-    rollThroughArray(json);
-	happy(json);
+function getJSONdata(clarification_typ)
+{ 
+	console.log(clarification_typ);
+	var json_string = "../json/" + clarification_typ;
+
+  $.getJSON(json_string, function(json) 
+	{
+  	//console.log("Content of JSON.file", json);
+  	rollThroughArray(json);
+		happy(json);
   });
 }
 
 //iterate through String Array
-function rollThroughArray(jsonObject)
-{
-    $.each(jsonObject,function(Index_cla, clarifications){
+function rollThroughArray(jsonObject){
+	document.getElementById('sampleArea').innerHTML = "";
+
+    $.each(jsonObject,function(Index_cla, clarifications)
+		{
       //console.log(Index_cla); //JSON Name
       //console.log(clarifications); //Shows the clarifications Object with all it´s sub Items
-      $.each(clarifications, function(clarification, index_y){
+      $.each(clarifications, function(clarification, index_y)
+			{
           //console.log(clarification); //Clarification Name
           //console.log(index_y); //Shows the Function Object with all it´s sub Items
-        $.each(index_y, function(Object_key, Object_value){ 
+        $.each(index_y, function(Object_key, Object_value)
+				{ 
             //console.log(Object_name); useless
-            var clarification_data = {
+            var clarification_data = 
+						{
 
               Clarification_Name:         Object_value.Headline,
               Clarification_Description:  Object_value.Description,
@@ -39,20 +48,22 @@ function rollThroughArray(jsonObject)
 }
 
 //create Arrays for stored function names
-function happy(jsonObject)
-{
+function happy(jsonObject){
 	console.log("hohoho");
 	var Clarification_counter = 0;
 	var Clarification_Array = [];
 	var Clarification_NameArray = [];
 	
-	$.each(jsonObject,function(Index_cla, clarifications){
-    	$.each(clarifications, function(clarification, index_y){
-			Clarification_counter = Clarification_counter + 1;
-	    	$.each(index_y, function(Object_key, Object_value){ 
-				Clarification_Array.push(Object_value.function_name);
-				Clarification_NameArray.push(Object_value.Headline);
-    		});
+	$.each(jsonObject,function(Index_cla, clarifications)
+	{
+  	$.each(clarifications, function(clarification, index_y)
+		{
+		Clarification_counter = Clarification_counter + 1;
+	   	$.each(index_y, function(Object_key, Object_value)
+			{ 
+			Clarification_Array.push(Object_value.function_name);
+			Clarification_NameArray.push(Object_value.Headline);
+    	});
 		});
 	});
 	//console.log(Clarification_counter);
@@ -65,9 +76,8 @@ function happy(jsonObject)
 
 //create a Mustache template
 //use Mustache to render a template
-function writeMustache(template, data, output_position)
-{
-    var output = Mustache.render(template, data);
+function writeMustache(template, data, output_position){
+  var output = Mustache.render(template, data);
 	var newcontent = output_position.innerHTML + output;
 
 	output_position.innerHTML = newcontent;
@@ -93,26 +103,56 @@ function appendClarifications(clarifications_functions, clarification_names, cou
 }
 
 //Simple Placeholder function
-function function_test()
-{
+function function_test(){
 	//alert("Juhu!");
 	hello();
 }
 
-function reload_open_tab()
-{
+function reload_open_tab(){
 	chrome.tabs.reload();
 }
 
-function hello() {
-  chrome.tabs.executeScript({
-    file: 'js/contentscript.js'
-  }); 
+function hello(){
+  chrome.tabs.executeScript(
+		{
+    	file: 'js/contentscript.js'
+  	}); 
 }
 
+//Ausgewählte Kategorie wird überprüft.
+function checkRadioValue(){
+	var json_value;
+
+	if(document.getElementById("kat1").checked)
+	{
+			json_value = document.getElementById("kat1").value;
+			console.log("String contains Value: " + json_value);
+			getJSONdata(json_value);
+
+	}
+	else if(document.getElementById("kat2").checked)
+	{
+			json_value = document.getElementById("kat2").value;
+			console.log("String contains Value: " + json_value);
+			getJSONdata(json_value);
+	}
+	else if(document.getElementById("kat3").checked)
+	{
+			json_value = document.getElementById("kat3").value;
+			console.log("String contains Value: " + json_value);
+			getJSONdata(json_value);
+	}
+}
+
+
 //Wait until document has loaded
-$(document).ready(function()
-{ 
+$(document).ready(function(){ 
 	//call starting function
-  	getJSONdata();
+	var categorys = document.getElementsByClassName("category_button");
+	for (var i=0; i < categorys.length; i++)
+	{
+		//console.log("Is something happening?");
+		//console.log(categorys[i]);
+		categorys[i].onclick = function(){ checkRadioValue();}
+	}
 });
