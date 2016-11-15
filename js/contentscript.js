37b295f7866aae73d93149013074687e7ad61cf6
Kirr <kirrkev@outlook.de>
2016-11-15 16:17:29 +0100

    First Steps for Page Translator!!!
    - Problem with "google not defined" for page.
    - error is probably in the script tag (maybe i have to wait before
    calling google)

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,3 +1,44 @@
-document.body.style.background = "white";
-//alert('hello ' + document.location.href);
+//First get all DOM-Elements with Text inside
 
+    var all = document.getElementsByTagName("h3");
+    var allelements = document.querySelectorAll('p,li')
+
+//Iterate through all captured DOM-Elements
+
+    console.log(all);
+
+    for (i = 0; i < all.length; i++) { 
+        //Here you should translate the text
+        console.log(all[i].textContent);
+    }
+
+//Translate via Translation-Service-API the DOM-Elements
+
+    //Before you translate the Text you have to load the Google-API into the document
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'http://www.google.com/jsapi?key=AIzaSyA5m1Nc8ws2BbmPRwKu5gFradvD_hgq6G0';
+    document.head.appendChild(script);
+    console.log(script);
+
+    //google ist not defined!!!!
+    google.load("language", "1");
+
+    function initialize() {
+
+        var all = document.getElementsByTagName("h3");            
+        //var text = all[0].textContent;
+        var text ="Hello World.";
+        console.log(text);
+
+        google.language.translate(text, 'en', 'es', function(result) {
+            var translated = document.getElementById("translation");
+            if (result.translation) {
+                console.log(result.translation);
+                }
+            });
+        }
+
+//Replace the old Text with the translated Text
+
+    //all[i].innerText = "Translated Text";
