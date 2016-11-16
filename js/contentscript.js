2ab326af7d5655d6a184528fb13460dc50c75bc8
Kirr <kirrkev@outlook.de>
2016-11-16 18:28:03 +0100

    Barriereverdeutlichung 1 | Language Replacer
    ChildNodes werden noch nicht richtig erkannt und es wird HTML-Content
    ersetzt, welcher eigentlich bestehen bleiben soll.

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,44 +1,51 @@
 //First get all DOM-Elements with Text inside
 
-    var all = document.getElementsByTagName("h3");
-    var allelements = document.querySelectorAll('p,li')
+    var search_terms = [
+        "a",
+        "p",
+        "span",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "img",
+        "li"
+    ]
 
-//Iterate through all captured DOM-Elements
+    var lorem_ipsum = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
 
-    console.log(all);
+    var all_elements_of_searchterm;
 
-    for (i = 0; i < all.length; i++) { 
-        //Here you should translate the text
-        console.log(all[i].textContent);
-    }
+    for (e = 0; e < search_terms.length; e++)
+    {
+        all_elements_of_searchterm = document.getElementsByTagName(search_terms[e]); 
 
-//Translate via Translation-Service-API the DOM-Elements
+        //Iterate through all captured DOM-Elements
 
-    //Before you translate the Text you have to load the Google-API into the document
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'http://www.google.com/jsapi?key=AIzaSyA5m1Nc8ws2BbmPRwKu5gFradvD_hgq6G0';
-    document.head.appendChild(script);
-    console.log(script);
+        console.log(all_elements_of_searchterm);
+        
+        for (i = 0; i < all_elements_of_searchterm.length; i++) { 
+            //Here you should translate the text
+            try 
+            {
+                var Length_of_Text = all_elements_of_searchterm[i].textContent.length;
+                var new_Text = "";
 
-    //google ist not defined!!!!
-    google.load("language", "1");
-
-    function initialize() {
-
-        var all = document.getElementsByTagName("h3");            
-        //var text = all[0].textContent;
-        var text ="Hello World.";
-        console.log(text);
-
-        google.language.translate(text, 'en', 'es', function(result) {
-            var translated = document.getElementById("translation");
-            if (result.translation) {
-                console.log(result.translation);
+                for(j = 0; j < Length_of_Text; j++)
+                {
+                    new_Text = new_Text + lorem_ipsum[j];
                 }
-            });
+
+                all_elements_of_searchterm[i].textContent = new_Text;    
+            }
+            catch(err) {
+                console.log("Element " + all_elements_of_searchterm[i] + "has no Textcontent." );
+                console.log(err);
+            }
         }
 
-//Replace the old Text with the translated Text
+    }
 
-    //all[i].innerText = "Translated Text";
+
