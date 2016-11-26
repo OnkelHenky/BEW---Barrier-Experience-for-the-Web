1c8a798f41e87c78634c4a13735c1b917958bb74
Kirr <kirrkev@outlook.de>
2016-11-26 01:56:37 +0100

    Architektur Update 1/2

--- js/popup.js
+++ js/popup.js
@@ -1,74 +1,76 @@
 //Wait until document has loaded
-$(document).ready(function(){ 
-	//call starting function
+$(document).ready(function()
+{ 
+	appendCategoryListeners();
+});
+
+//Append Listeners to Category Radiobuttons
+function appendCategoryListeners(){
 	var categorys = document.getElementsByClassName("category_input");
+	
 	for (var i=0; i < categorys.length; i++)
 	{
-		//console.log("Is something happening?");
-		console.log(categorys[i]);
 		categorys[i].onchange = function(){ checkRadioValue();}
+		console.log("Listener für Radionbutton " + categorys[i] + "wurde hinzugefügt.");
 	}
-});
+
+}
+
+function appendBarrierListeners(){
+	var barriers = document.getElementsByClassName("clarification_radiobuttons");
+
+	for (var i=0; i < barriers.length; i++)
+	{
+		//barriers[i].onchange = function(){ //checkRadioValue();}
+		console.log("Listener für Radionbutton " + barriers[i] + " wurde hinzugefügt.");
+		console.log(barriers[i].id);
+	}
+	
+}
 
 //Ausgewählte Kategorie wird überprüft.
 function checkRadioValue(){
-	console.log("1: Radio value wird abgefragt.")
-	var json_value;
+	console.log("Radio value wird abgefragt.")
+	var json_category;
 	var headline_values = document.getElementsByClassName("category_label");
 
 	//Das onclick event wird vor dem radio-button wechsel gestartet.
-
 	if(document.getElementById("kat1").checked)
 	{
-		json_value = document.getElementById("kat1").value;
-		//console.log("String contains Value: " + json_value);			
+		json_category = document.getElementById("kat1").value;		
 		document.getElementById("clarifications_headline").innerText = headline_values[0].innerText + " clarifications:";
-		document.getElementById("clarifications_infotext").innerText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-		//json_value + " clarifications:";
-		console.log("2: JSON.data wird geladen.")
-		getJSONdata(json_value);
-
+		document.getElementById("clarifications_infotext").innerText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";		
 	}
 	else if(document.getElementById("kat2").checked)
 	{
 		json_value = document.getElementById("kat2").value;
 		document.getElementById("clarifications_headline").innerText = headline_values[1].innerText + " clarifications:";
-		document.getElementById("clarifications_infotext").innerText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-		console.log("2: JSON.data wird geladen.")
-		getJSONdata(json_value);
+		document.getElementById("clarifications_infotext").innerText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
 	}
 	else if(document.getElementById("kat3").checked)
 	{
 		json_value = document.getElementById("kat3").value;
 		document.getElementById("clarifications_headline").innerText = headline_values[2].innerText + " clarifications:";
-		document.getElementById("clarifications_infotext").innerText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-		console.log("2: JSON.data wird geladen.")
-		getJSONdata(json_value);
+		document.getElementById("clarifications_infotext").innerText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
 	}
 	else
 	{
-		console.log("2: no radio-button is selected.");
+		console.log("Kein Radiovalue konnte ausgewählt werden.");
+		return;
 	}
+
+	console.log("JSOn File wurde ausgewählt.");
+	//Text for later usage
+	document.getElementById("category_value").innerText = json_category;
+	document.getElementById("category_value").style.display = "none";
+	openJSON_forBarriers(json_category);
 }
 
-//Parse JSON to String Array
-function getJSONdata(clarification_typ)
-{ 
-	console.log(clarification_typ);
-	var json_string = "../json/" + clarification_typ;
-
-  $.getJSON(json_string, function(json) 
-	{
-  		console.log("3: Content of JSON.file", json);
-  		rollThroughArray(json);
-			happy(json);
-  });
-}
-
-//iterate through String Array
-function rollThroughArray(jsonObject){
-	console.log("4: Buttons werden erstellt.")
-	document.getElementById('sampleArea').innerHTML = "";
+//Buttons für einzelne Barrieren der ausgewählten Kategorien erstellen.
+function create_barrier_buttons(jsonObject){
+	console.log("Barriere Buttons werden erstellt.");
+	console.log(jsonObject);
+	document.getElementById('barrier_button_area').innerHTML = "";
 
     $.each(jsonObject,function(Index_cla, clarifications)
 		{
@@ -83,20 +85,18 @@ function rollThroughArray(jsonObject){
             //console.log(Object_name); useless
             var clarification_data = 
 						{
-
-              Clarification_Name:         Object_value.Headline,
-              Clarification_Description:  Object_value.Description,
-							Clarification_Information:	Object_value.Helping_information,
-              Clarification_Image_Source: Object_value.Image_source,
-              Clarification_Image_Alt:    Object_value.Image_alt,
-              Clarification_Function:     Object_value.function_name //at the moment not in use
-            };
+							Clarification_Name:         Object_value.Headline,
+              				Clarification_Description:  Object_value.Description,
+              				Clarification_Image_Source: Object_value.Image_source,
+              				Clarification_Image_Alt:    Object_value.Image_alt,
+            			};
 
             var template = "<div class='clarification_div'><input type='radio' id='{{Clarification_Name}}' name='clarification' value='{{Clarification_Description}}' class='clarification_radiobuttons'><label class='clarification_button' for='{{Clarification_Name}}'><img src='{{Clarification_Image_Source}}' alt='{{Clarification_Image_Alt}}'></label><label class='clarification_label'>{{Clarification_Name}}</label><div>";
             
-            var output_position = document.getElementById('sampleArea');
+            var output_position = document.getElementById('barrier_button_area');
             
             writeMustache(template, clarification_data, output_position, 0);
+			appendBarrierListeners();
         });
       });
     });
@@ -110,17 +110,18 @@ function writeMustache(template, data, output_position, type){
 	if(type == 0)
 	{
 			output_position.innerHTML = output_position.innerHTML + output;
-			console.log("5: mustache template wird erstellt.")
+			console.log("Mustache template wird erstellt.")
+			document.getElementById("clarifications").style.display = "inline";
 	}
 	else if(type == 1)
 	{
 			output_position.innerHTML = output;
-			console.log("mustache template für infobox wird erstellt.")
-	}
-
-	
+			console.log("Mustache template für infobox wird erstellt.")
+			document.getElementById("clarification_details").style.display = "inline";
+	}	
 }
 
+/*
 //create Arrays for stored function names
 function happy(jsonObject){
 	var Clarification_counter = 0;
@@ -140,8 +141,6 @@ function happy(jsonObject){
     		});
 		});
 	});
-
-
 
 	appendClarifications(Clarification_Array, Clarification_NameArray, Clarification_counter);
 }
@@ -171,6 +170,7 @@ function appendClarifications(clarifications_functions, clarification_names, cou
 
 	}
 }
+*/
 
 function show_clarification_details()
 {
@@ -185,19 +185,6 @@ function show_clarification_details()
 //Simple Placeholder function
 function function_test(){
 	alert("You could start a clarification with this button.");
-}
-
-function check_https(){
-	var bool;
-	if(location.protocol == 'https:')
-	{
-		bool = true;
-	}
-	else
-	{
-		bool = false;
-	}
-	return bool;
 }
 
 function security_hint() {
@@ -245,4 +232,32 @@ function operable1(){
 			file: 'js/perception1.js'
 		}); 
   }
+}
+
+//??????
+function openJSON_forBarriers(category_value)
+{	
+	console.log("Die openJSON function wurde gestartet.");
+	console.log("Ausgewählte Kategorie: " + category_value);
+
+	var json_string = "../json/" + category_value;
+  	$.getJSON(json_string, function(json) 
+		{
+			console.log(json);
+			create_barrier_buttons(json);
+  		});
+}
+
+//??????
+function openJSON_forInformations(category_value)
+{	
+	console.log("Die openJSON function wurde gestartet.");
+	console.log("Ausgewählte Kategorie: " + category_value);
+
+	var json_string = "../json/" + category_value;
+  	$.getJSON(json_string, function(json) 
+		{
+			console.log(json);
+			//Informationen anzeigen (json);
+  		});
 }
